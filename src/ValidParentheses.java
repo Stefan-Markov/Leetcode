0ef720423e0c460362917c5fa8126f7dba307677
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2022-10-24 17:07:54 +0300

    solution

---
+++ src/ValidParentheses.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String input = "()";
+        boolean valid = isValid(input);
+        System.out.println(valid);
+    }
+
+
+    public static boolean isValid(String s) {
+        Deque<Character> st = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') st.push(')');
+            else if (c == '[') st.push(']');
+            else if (c == '{') st.push('}');
+            else if (st.isEmpty() || st.pop() != c)
+                return false;
+        }
+        return st.isEmpty();
+    }
+}
