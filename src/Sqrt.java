ac73a6b53acb143395e5da1f5da32e74e36396fd
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2022-10-31 12:49:23 +0200

    solution

---
+++ src/Sqrt.java
@@ -0,0 +1,24 @@
+public class Sqrt {
+    public static void main(String[] args) {
+
+        int x = 4;
+        int i = mySqrt(x);
+        System.out.println(i);
+
+    }
+
+    public static int mySqrt(int x) {
+        if (x == 0) return 0;
+        int start = 1, end = x;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1))
+                return mid;
+            else if (mid > x / mid)
+                end = mid;
+            else
+                start = mid + 1;
+        }
+        return start;
+    }
+}
